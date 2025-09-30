c24dc426ac4a11d5441bef9bb3104b80e22ddc8f
ukasha-dev <ukasha@nextpak.org>
2025-09-30 14:41:24 +0500

    Fixed Redirects

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import * as SecureStore from '../utils/secureStore';
 
-const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.1.6:3001';
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const ACCESS_TOKEN_KEY = 'gg_access_token';
 const REFRESH_TOKEN_KEY = 'gg_refresh_token';
